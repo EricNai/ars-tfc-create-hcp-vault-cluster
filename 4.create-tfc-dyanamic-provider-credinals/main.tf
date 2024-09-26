215ad5f234d98dde74f48d0f5e9a4e4b8300e472
EricNai <naing07@gmail.com>
2024-09-26 14:39:53 +0000

    change organization name  dynamic credential main tf file line 81

--- 4.create-tfc-dyanamic-provider-credinals/main.tf
+++ 4.create-tfc-dyanamic-provider-credinals/main.tf
@@ -78,7 +78,7 @@ resource "vault_jwt_auth_backend_role" "jwt_role" {
   bound_audiences = ["vault.workload.identity"]
   bound_claims_type = "glob"
   bound_claims = {
-    sub = "organization:ars_secureInfraPrj:project:ars_secureInfraPrj:workspace:*:run_phase:*"
+    sub = "organization:ars-SecureOps:project:ARS-AWS-Secret-Engine-Vault-Cluster:workspace:*:run_phase:*"
   }
   user_claim      = "terraform_full_workspace"
   role_type       = "jwt"
